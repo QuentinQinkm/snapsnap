7ebe34b4f4bec3a440dfe3d98a1425f3f740895c
kq62 <kq62@cornell.edu>
2025-08-08 15:22:35 -0400

    Initial Commit

---
+++ SnapSnap/SnapSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSnapApp.swift
+//  SnapSnap
+//
+//  Created by Kuangming Qin on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
